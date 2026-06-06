5bb06716f76f1c78794fa1f1d7c0f78610ede53d
christensenandrew455-cell <christensenandrew455@gmail.com>
2026-06-06 15:56:33 -0400

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -75,7 +75,7 @@ export default function ContactPage() {
           <p>
             <strong>Email:</strong> {config.email}
           </p>
-          )}
+          )
         </div>
 
         <form
